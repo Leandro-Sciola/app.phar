ab03e4702217d1f0b043344b122a2e971b2cb239
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2023-03-15 13:25:07 -0300

    Create

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<?php include_once 'phar.php'; server('localhost', 8000); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>App</title>
+</head>
+<body>
+<h1>Welcome!</h1>
+</body>
+</html>
