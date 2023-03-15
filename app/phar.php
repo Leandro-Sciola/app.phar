441f06094fe8af8148d3cf1742f4297826ac28ef
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2023-03-15 13:25:56 -0300

    Create

---
+++ app/phar.php
@@ -0,0 +1,66 @@
+#!/usr/bin/env php
+<?php
+
+if (!pathinfo(basename(__DIR__), PATHINFO_EXTENSION)) {
+    if (!isset($argv[1])) {
+        echo shell_exec('php --define phar.readonly=0 ' . __DIR__ .
+                        '/phar.php ' . basename(__DIR__) . '.phar');
+    } else {
+        compile($argv[1]);
+    }
+} else {
+    define('APP_NAME', basename(__DIR__));
+}
+
+function compile($pharFile) {
+    try {
+        // clean up
+        if (file_exists($pharFile)) {
+            unlink($pharFile);
+        }
+
+        if (file_exists($pharFile . '.gz')) {
+            unlink($pharFile . '.gz');
+        }
+
+        // create phar
+        $phar = new Phar($pharFile);
+
+        // start buffering. Mandatory to modify stub to add shebang
+        $phar->startBuffering();
+
+        // Create the default stub from main.php entrypoint
+        $defaultStub = $phar->createDefaultStub('index.php');
+
+        // Add the rest of the apps files
+        $phar->buildFromDirectory(__DIR__);
+
+        // Customize the stub to add the shebang
+        $stub = "#!/usr/bin/env php \n" . $defaultStub;
+
+        // Add the stub
+        $phar->setStub($stub);
+
+        $phar->stopBuffering();
+
+        // plus - compressing it into gzip  
+        $phar->compressFiles(Phar::GZ);
+
+        # Make the file executable
+        chmod(getcwd() . "/{$pharFile}", 0770);
+
+        echo "$pharFile successfully created" . PHP_EOL;
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+function server($host, $port) {
+    if (defined('APP_NAME')) {
+        exec("(lsof -Pi :{$port} -sTCP:LISTEN -t >/dev/null) || " .
+             "php -S {$host}:{$port} " . APP_NAME);
+        ob_get_clean();
+    } else {
+        exit;
+    }
+}
